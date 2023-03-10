d592b3579434c283d9c6218e21ff7ab066f56fcc
dearpollapat <pollapat.dear@gmail.com>
2023-03-11 05:17:16 +0700

    getStaticPaths and dynamic route
    :(

---
+++ nextjs-th-101/pages/products/[id].js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import styles from "@/styles/Detail.module.css";
+import Image from "next/image";
+
+export async function getStaticPaths() {
+  const res = await fetch("https://dummyjson.com/products?limit=12");
+  const data = await res.json();
+  const paths = data.products.map((item) => {
+    return {
+      params: { id: String(item.id) },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const id = params.id;
+  const res = await fetch(`https://dummyjson.com/products/${id}`);
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: { product: data },
+  };
+}
+
+export default function ProductDetail({ product }) {
+  return (
+    <>
+      <Head>
+        <title>{product.title}</title>
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <Image
+            src={product.thumbnail}
+            width={300}
+            height={300}
+            alt={product.title}
+          />
+        </div>
+        <div className={styles.detail}>
+          <h1>Product Name : {product.title}</h1>
+          <h2>Price : {product.price}</h2>
+          <h2>Category : {product.category}</h2>
+          <h3>Brand : {product.brand}</h3>
+          <h4>Description : {product.description}</h4>
+          <h4>Rating : {product.rating}</h4>
+        </div>
+      </div>
+    </>
+  );
+}
--- nextjs-th-101/pages/products/index.js
+++ nextjs-th-101/pages/products/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import styles from "@/styles/Product.module.css";
+import Link from "next/link";
 
 export async function getStaticProps() {
   const res = await fetch("https://dummyjson.com/products?limit=12");
@@ -16,16 +18,20 @@ export default function Index({ products }) {
   return (
     <>
       <h1>all products</h1>
-      <div>
+      <div className={styles.container}>
         {products.map((item) => (
           <div key={item.id}>
-            <h2>{item.title}</h2>
-            <Image
-              src={item.thumbnail}
-              width={300}
-              height={300}
-              alt={item.title}
-            />
+            <Link href={"/products/" + item.id}>
+              <h2 className={styles.title}>
+                {item.title} | {item.price}
+              </h2>
+              <Image
+                src={item.thumbnail}
+                width={300}
+                height={300}
+                alt={item.title}
+              />
+            </Link>
           </div>
         ))}
       </div>
